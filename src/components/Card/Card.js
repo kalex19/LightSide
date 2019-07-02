f19e34124ce4f79757435f3921e51afc36088d91
Kayla <kayla.larson1990@gmail.com>
2019-07-02 16:10:31 -0600

    Figure out how to separate data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,13 +5,23 @@ import PropTypes from 'prop-types';
 const Card = (props) => {
     return (
         <section className="card">
-            <h2>Name: {props.name}</h2>
+            <h2>
+                Name: {props.name}
+            </h2>
             {console.log(props.terrain)}
             <button className='favorited'><span role="img">🤩</span></button>
-            <h2>{props.birthyear || props.terrain || props.model}</h2>
-            <h2>{props.gender || props.diameter || props.vehicle_class}</h2>
-            <h2>{props.height || props.population || props.passengers}</h2>
-            <h2>{props.eyecolor}</h2>
+            <h2>
+                {props.birthyear || props.terrain || props.model}
+            </h2>
+            <h2>
+                {props.gender || props.diameter || props.vehicle_class}
+            </h2>
+            <h2>
+                {props.height || props.population || props.passengers}
+            </h2>
+            <h2>
+                {props.eyecolor}
+            </h2>
         </section>
     )
 }
--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -6,57 +6,41 @@ const Container = ({
     data
 }) => {
     const findData = data.map(item => {
-        {console.log(data)}
-
         return <Card
-<<<<<<< HEAD
-            name={item.name}
-            birthyear={item.birth_year}
-            gender={item.gender}
-            height={item.height}
-            eyecolor={item.eye_color}
-            model={item.model}
-            terrain={item.terrain}
-            diameter={item.diameter}
-            population={item.population}
-            class={item.vehicle_class}
-            passengers={item.passengers}
-=======
-        name = {
-            item.name
-        }
-        birthyear = {
-            item.birth_year
-        }
-        gender = {
-            item.gender
-        }
-        height = {
-            item.height
-        }
-        eyecolor = {
-            item.eye_color
-        }
-        model = {
-            item.model
-        }
-        class = {
-            item.vehicle_class
-        }
-        passengers = {
-            item.passengers
-        }
-        favorites = {
-            data.favorites
-        }
->>>>>>> 69e111519ed6b53133c5c53841204500a45ca389
+            name = {
+                item.name
+            }
+            birthyear = {
+                item.birth_year
+            }
+            gender = {
+                item.gender
+            }
+            height = {
+                item.height
+            }
+            eyecolor = {
+                item.eye_color
+            }
+            model = {
+                item.model
+            }
+            class = {
+                item.vehicle_class
+            }
+            passengers = {
+                item.passengers
+            }
+            favorites = {
+                data.favorites
+            }
         />
     })
-    return ( <
-        main className = "card-container" > {
-            findData
-        } <
-        /main>
+    
+    return ( 
+        <main className="card-container"> 
+            { findData } 
+        </main>
     )
 }
 
