841a7a6ba956ba1bd65504f6e95539fe5343489d
Kayla <kayla.larson1990@gmail.com>
2019-07-01 20:10:47 -0600

    Add functionality to Card, Container, and App.js Try to dive into data and implement Router

---
+++ src/components/App/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import App from './App';
+
+
+describe('App', () => {
+    let wrapper;
+    
+    beforeEach(() => {
+        wrapper = shallow(<App />);
+    });
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    
+})
