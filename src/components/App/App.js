54943aae821535f013c15b422563b30779883f69
Katherine Lewis <kale8432@colorado.edu>
2019-07-02 11:14:06 -0600

    Moved state and container rendering to header so scroll text can be rendered before header and header can render containers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,77 +1,13 @@
-import React, { Component } from 'react';
 import './App.css';
-import Header from '../Header/Header.js';
-import Container from '../Container/Container.js';
-// import Card from '../Card/Card.js'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import PeopleData from '../Card/PeopleData.js'
-import PlanetsData from '../Card/PlanetsData.js'
-import VehiclesData from '../Card/VehiclesData.js'
+import Header from '../Header/Header';
+import ScrollText from '../ScrollText/ScrollText';
+import React from 'react';
 
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      people: PeopleData.results,
-      planet: PlanetsData.results,
-      vehicle: VehiclesData.results,
-      favorites: ''
-    }
-  }
-  render() {
-    const People = () => (
-      <div>
-        <Container data={this.state.people} />
-      </div>
-    );
-
-    const Planets = () => (
-      <div>
-        <Container data={this.state.planet} />
-      </div>
-    );
-
-    const Vehicles = () => (
-      <div>
-        <Container data={this.state.vehicle} />
-      </div>
-    )
-
-    return (
-      <div className="App">
-        <header className="lightside-header">
-          <h1>Light Side</h1>
-          <button className="btns favorited">Favorited: <span>0</span></button>
-          {/* <Header /> */}
-        </header>
-        <Router>
-          <div className="change-btn">
-              <Link to='/People'>
-                <button className="btns people-btn">People</button>
-              </Link>
-              <Link to='/Planets'>
-                <button className="btns planets-btn">Planets</button>
-              </Link>
-              <Link to='/Vehicles'>
-                <button className="btns vehicles-btn">Vehicles</button>
-              </Link>
-            </div>
-            <Route path='/People' component={People} />
-            <Route path='/Planets' component={Planets} />
-            <Route path='/Vehicles' component={Vehicles} />
-          </Router>
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello!</h1>
-          {console.log(PeopleData.results)}
-          {console.log(PlanetsData.results)}
-          {console.log(VehiclesData.results)}
-        </header>
-      </div>
-    )
-  };
+export default function App(){
+	return (
+		<div className="App">
+			<ScrollText />
+			<Header />
+		</div>
+	);
 }
-
-export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,66 @@
-import React from 'react';
 import './Header.css';
+import Container from '../Container/Container';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
-const Header = () => {
-    return (
-        <div className="btn-container">
-            
-        </div>
-    )
+export class Header extends Component {
+	constructor() {
+		super();
+		this.state = {
+			people: [],
+			planet: [],
+			vehicle: [],
+			favorites: [],
+			error: ''
+		};
+
+		const People = () => (
+			<div>
+				<Container data={this.state.people} /> {' '}
+			</div>
+		);
+
+		const Planets = () => (
+			<div>
+				<Container data={this.state.planet} /> {' '}
+			</div>
+		);
+
+		const Vehicles = () => (
+			<div>
+				<Container data={this.state.vehicle} /> {' '}
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				<header className="lightside-header">
+					<h1> Light Side </h1>{' '}
+					<button className="btns favorited">
+						Favorited: <span> 0 </span>{' '}
+					</button>{' '}
+				</header>{' '}
+				<Router>
+					<div className="change-btn">
+						<Link to="/People">
+							<button className="btns people-btn"> People </button>{' '}
+						</Link>{' '}
+						<Link to="/Planets">
+							<button className="btns planets-btn"> Planets </button>{' '}
+						</Link>{' '}
+						<Link to="/Vehicles">
+							<button className="btns vehicles-btn"> Vehicles </button>{' '}
+						</Link>{' '}
+					</div>{' '}
+					<Route path="/People" component={People} />
+					<Route path="/Planets" component={Planets} />
+					<Route path="/Vehicles" component={Vehicles} />{' '}
+				</Router>{' '}
+			</div>
+		);
+	}
 }
 
 export default Header;
