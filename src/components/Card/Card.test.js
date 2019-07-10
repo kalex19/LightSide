bbafcc4fa5f619f42f26aff29e30fe50106eaa87
Kayla <kayla.larson1990@gmail.com>
2019-07-10 08:49:21 -0600

    Fix spacing

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -5,39 +5,40 @@ import {
 import Card from './Card.jsx';
 
 describe('Card', () => {
-            let mockFunc;
-            let mockData;
+  let mockFunc;
+  let mockData;
 
-            beforeEach(() => {
-                mockFunc = jest.fn()
-                mockData = {
-                    class: "starfighter",
-                    id: "2014-12-10T16:33:52.860000Z",
-                    model: "Twin Ion Engine/Ln Starfighter",
-                    name: "TIE/LN starfighter",
-                    passengers: "0"
-                }
-            })
-            it('should match snapshot', () => {
-                    const wrapper = shallow( < Card info = {
-                            mockData
-                        }
-                        favoriteCard = {
-                            mockFunc
-                        }
-                        />)
-                        expect(wrapper).toMatchSnapshot()
-                    });
+  beforeEach(() => {
+    mockFunc = jest.fn()
+    mockData = {
+        class: "starfighter",
+        id: "2014-12-10T16:33:52.860000Z",
+        model: "Twin Ion Engine/Ln Starfighter",
+        name: "TIE/LN starfighter",
+        passengers: "0"
+    }
+  })
+  it('should match snapshot', () => {
+    const wrapper = shallow( < Card info = {
+            mockData
+        }
+        favoriteCard = {
+            mockFunc
+        }
+        />)
+    expect(wrapper).toMatchSnapshot()
+  });
 
-                it('should call favorite card on click', () => {
-                        const wrapper = shallow( < Card info = {
-                                mockData
-                            }
-                            favoriteCard = {
-                                mockFunc
-                            }
-                            />)
+  it('should call favorite card on click', () => {
+    const wrapper = shallow( < Card 
+            info = {
+                mockData
+            }
+            favoriteCard = {
+                mockFunc
+            }
+    />)
 
-                            mockFunc("2014-12-10T16:33:52.860000Z"); wrapper.find('button').simulate('click'); expect(mockFunc).toBeCalledWith(mockData.id);
-                        });
-                })
+    mockFunc("2014-12-10T16:33:52.860000Z"); wrapper.find('button').simulate('click'); expect(mockFunc).toBeCalledWith(mockData.id);
+    });
+})
